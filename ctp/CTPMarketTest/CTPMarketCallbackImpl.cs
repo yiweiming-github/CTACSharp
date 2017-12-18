3d8633fe9b3a8d444c2320a18a651bb8c45cc78e
yiweiming@gmail.com <yiweiming@gmail.com>
2017-12-18 17:08:56 +0800

    add CTP market swig projects

---
+++ ctp/CTPMarketTest/CTPMarketCallbackImpl.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTPMarketTest
+{
+    class CTPMarketCallbackImpl : CThostFtdcMdSpi
+    {
+        public bool IsReady
+        {
+            get { return _isReady; }
+        }
+
+        private bool _isReady = false;
+
+        public bool IsConnected
+        {
+            get { return _isConnected; }
+        }
+
+        private bool _isConnected = false;
+
+        public override void OnFrontConnected()
+        {
+            Console.WriteLine("OnFrontConnected");
+            _isConnected = true;
+        }
+
+        public override void OnRspUserLogin(CThostFtdcRspUserLoginField pRspUserLogin, CThostFtdcRspInfoField pRspInfo, int nRequestID,
+            bool bIsLast)
+        {
+            Console.WriteLine("OnRspUserLogin");
+            _isReady = true;
+        }
+
+        public override void OnRspError(CThostFtdcRspInfoField pRspInfo, int nRequestID, bool bIsLast)
+        {
+            Console.WriteLine("OnRspError");
+        }
+
+        public override void OnRspSubMarketData(CThostFtdcSpecificInstrumentField pSpecificInstrument, CThostFtdcRspInfoField pRspInfo,
+            int nRequestID, bool bIsLast)
+        {
+            if (pRspInfo.ErrorID != 0)
+            {
+                Console.WriteLine("OnRspSubMarketData Error: {0}", pRspInfo.ErrorMsg);
+            }
+            else
+            {
+                Console.WriteLine("OnRspSubMarketData");
+            }
+        }
+
+        public override void OnRtnDepthMarketData(CThostFtdcDepthMarketDataField pDepthMarketData)
+        {
+            if (pDepthMarketData != null)
+            {
+                Console.WriteLine("OnRtnDepthMarketData {0} {1} {2}", pDepthMarketData.InstrumentID,
+                    pDepthMarketData.UpdateTime, pDepthMarketData.LastPrice);
+            }
+            else
+            {
+                Console.WriteLine("Empty market data");
+            }
+        }
+
+        public override void OnFrontDisconnected(int nReason)
+        {
+            Console.WriteLine("OnFrontDisconnected reason: {0}", nReason);
+        }
+
+        public override void OnRspUserLogout(CThostFtdcUserLogoutField pUserLogout, CThostFtdcRspInfoField pRspInfo, int nRequestID,
+            bool bIsLast)
+        {
+            Console.WriteLine("OnRspUserLogout");
+        }
+    }
+}
---
+++ ctp/CTPMarketTest/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace CTPMarketTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ctpServerAddress = "tcp://ctp1-md1.citicsf.com:41213";
+
+            var api = CThostFtdcMdApi.CreateFtdcMdApi();
+            var spi = new CTPMarketCallbackImpl();
+            api.RegisterSpi(spi);
+            api.RegisterFront(ctpServerAddress);
+            api.Init();
+            //api.Join();
+
+            while (!spi.IsConnected)
+            {
+                Thread.Sleep(100);
+                Console.WriteLine("is not connected...");
+            }
+
+            var userLogin = new CThostFtdcReqUserLoginField();
+            userLogin.BrokerID = "66666";
+            userLogin.UserID = "11782";
+            userLogin.Password = "358492";
+
+            int requestId = 0;
+            api.RegisterNameServer(ctpServerAddress);
+            api.Init();
+            api.ReqUserLogin(userLogin, requestId++);
+
+            while (!spi.IsReady)
+            {
+                Thread.Sleep(100);
+                Console.WriteLine("is not ready...");
+            }
+
+            var codes = new string[]
+            {
+                "RB1801",
+                "IF1803",
+                "TF1803"
+            };
+            api.SubscribeMarketData(codes, codes.Length);
+
+            while (true)
+            {
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
